174b35edc9a3452465d74609ada82f45a4fbaef7
Connor <connorlangdon451@yahoo.com>
2021-10-20 18:38:18 -0400

    Correct an error on the events page

--- events/index.php
+++ events/index.php
@@ -8,7 +8,7 @@
     <div class="row">
       <image src="/img/events/MN_Replay_v5.jpg" width="500" height="600">
       <image src="/img/events/NailString.jpg" width="500" height="600">
-      <image src="/img/events/Book_Binding_Makenight.jpg" width="500" height="600">
+      <image src="/img/events/Book_Binding_Makenight_v2.jpg" width="500" height="600">
       <image src="/img/events/yooper_keychain_makenight.jpg" width="500" height="600">
       <image src="/img/events/MakeMugKnight.jpg" width="500" height="600">
     </div>
