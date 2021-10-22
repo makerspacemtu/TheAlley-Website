47805528c63658d46acbed2e4a56c6502d152d3d
Connor <connorlangdon451@yahoo.com>
2021-10-22 17:19:07 -0400

    Adds a new "Plans" feature to the website but doesn't add it to the nav bar.

---
+++ plans/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<?php include '/app/_includes/head.php'; ?>
+<body>
+<?php include '/app/_includes/headerMain.php'; ?>
+<section>
+  <div class="content">
+    <div class="row">
+      <div class="planItem">
+        <p> Model Portage Canal Lift Bridge </p>
+        <image src="/app/img/projects/LiftBridge.jpg" width="300px" height="400px"> </br>
+          <a href="/app/PDFs/Portage_Canal_Lift_Bridge.pdf" download="Portage_Canal_Lift_Bridge" id="PDF"> Download PDF </a>
+  	      <a href="/app/ZIPs/Portage_Canal_Lift_Bridge.zip" download="Portage_Canal_Lift_Bridge"> Download DXF </a>
+      </div>
+    </div>
+  </div>
+</section>
+<!--section required for every layout-->
+<?php require '/app/_includes/call_to_action.php';
+      include '/app/_includes/footer.php';
+?>
+</body>
+</html>
