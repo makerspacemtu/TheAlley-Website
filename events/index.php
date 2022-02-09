1fa08b1ce60ca46d4b388a5f1bbb83664f56060a
Connor <connorlangdon451@yahoo.com>
2022-02-08 23:51:04 -0500

    Updates the onclick event for the events in the events tab

--- events/index.php
+++ events/index.php
@@ -15,7 +15,7 @@
       <div class="row">
         <h2 class="header"> Spring 2022 Events </h2>
         <p> click an event to see more details </p>
-        <image src="/img/events/BadRobotNight.jpg" width="500" height="600" onclick="openEvent(BadRobotNight)">
+        <image src="/img/events/BadRobotNight.jpg" width="500" height="600" onclick="openEvent('BadRobotNight')">
       </div>
     </div>
     <div class="content">
