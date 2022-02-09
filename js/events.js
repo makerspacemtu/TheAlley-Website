8968fb7cb0a1ad07e0ebc28ba05e19bf1264d4ec
Connor <connorlangdon451@yahoo.com>
2022-02-09 16:44:05 -0500

    fixes an issue with open event function/var name

--- js/events.js
+++ js/events.js
@@ -5,7 +5,7 @@ open the event overlay for that event.
 parameters: name- the ID of the event overlay that should open when the event is
 clicked
 */
-var openEvent;
+var openedEvent;
 function openEvent(name){
   //turn on the grey background
   document.getElementById("overlayBackground").style.display = "flex";
@@ -18,6 +18,6 @@ Open event will close an open event
 */
 function closeEvent(){
   document.getElementById("overlayBackground").style.display = "none";
-  document.getElementById(openEvent).style.display = "none";
-  openEvent = null;
+  document.getElementById(openedEvent).style.display = "none";
+  openedEvent = null;
 }
