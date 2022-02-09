5577b5b98d6443f60f1ab36f0a3b47631e6f6813
Connor <connorlangdon451@yahoo.com>
2022-02-08 23:35:11 -0500

    Adds some code to allow for an overlay when certain events are clicked

--- events/index.php
+++ events/index.php
@@ -3,25 +3,32 @@
 <?php include '/app/_includes/head.php'; ?>
 <body>
 <?php include '/app/_includes/headerMain.php'; ?>
-<section>
-  <div class="content">
-    <div class="row">
-      <h2 class="header"> Spring 2022 Events </h2>
-      <p> click an event to see more details </p>
-    </div>
+  <div class="overlayBackground">
   </div>
-  <div class="content">
-    <div class="row">
-      <h2 class="header"> Old Events </h2>
-      <image src="/img/events/BadRobotNight.jpg" width="500" height="600">
-      <image src="/img/events/MN_Replay_v5.jpg" width="500" height="600">
-      <image src="/img/events/NailString.jpg" width="500" height="600">
-      <image src="/img/events/Book_Binding_Makenight_v2.jpg" width="500" height="600">
-      <image src="/img/events/yooper_keychain_makenight.jpg" width="500" height="600">
-      <image src="/img/events/MakeMugKnight.jpg" width="500" height="600">
-    </div>
+
+  <div class="eventOverlay">
   </div>
-</section>
+
+  <section>
+    <div class="content">
+      <div class="row">
+        <h2 class="header"> Spring 2022 Events </h2>
+        <p> click an event to see more details </p>
+        <image src="/img/events/BadRobotNight.jpg" width="500" height="600" onclick="">
+      </div>
+    </div>
+    <div class="content">
+      <div class="row">
+        <h2 class="header"> Old Events </h2>
+        <image src="/img/events/BadRobotNight.jpg" width="500" height="600">
+        <image src="/img/events/MN_Replay_v5.jpg" width="500" height="600">
+        <image src="/img/events/NailString.jpg" width="500" height="600">
+        <image src="/img/events/Book_Binding_Makenight_v2.jpg" width="500" height="600">
+        <image src="/img/events/yooper_keychain_makenight.jpg" width="500" height="600">
+        <image src="/img/events/MakeMugKnight.jpg" width="500" height="600">
+      </div>
+    </div>
+  </section>
 <!--section required for every layout-->
 <?php require '/app/_includes/call_to_action.php';
       include '/app/_includes/footer.php';
---
+++ js/events.js
@@ -0,0 +1,12 @@
+/*
+Open event will take the name of an event(usually the one that was clicked) and
+open the event overlay for that event.
+
+Arguments: name- the ID of the event overlay that should open when the event is
+clicked
+*/
+var openEvent;
+function openEvent(name){
+  //turn on the grey background
+  document.getElementById("overlayBackground").style.display = "block";
+}
